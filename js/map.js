ba32c1f41d006f5c62d859948ee6084344c82d4b
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-06-16 21:35:05 +0300

    #13: Created DOM-elements for pins & ads, filled with data from the array of generated object, rendered DOM-elements

--- js/map.js
+++ js/map.js
@@ -21,9 +21,15 @@ var LOCATION_X_MIN = 300;
 var LOCATION_X_MAX = 900;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
+// var MAP_PIN_WIDTH = 40;
+// var MAP_PIN_HEIGHT = 40;
 
 var map = document.querySelector('.map');
+var pinList = document.querySelector('.map__pins');
+var TemplatePin = document.querySelector('template').content.querySelector('.map__pin');
 
+var TemplateAd = document.querySelector('template').content.querySelector('.map__card');
+var nextSibling = document.querySelector('.map__filters-container');
 
 // Возвращает случайное целое число из диапозона min и max;
 var getRandomIntByRange = function (min, max) {
@@ -36,36 +42,92 @@ var getRandomArrayItem = function (arr) {
 };
 
 // Возварщает массив из объектов. Принимает количество объектов.
-var createArreyOfObject = function (numberOfObject) {
+var getArreyOfObject = function (numberOfObject) {
   var array = [];
   for (var i = 0; i < numberOfObject; i++) {
     array.push({
-      'author': {
-        'avatar': 'img/avatars/userxx.png'
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
-      'offer': {
-        'title': getRandomArrayItem(TITLE),
-        'address': 'location.x, location.y',
-        'price': getRandomIntByRange(MIN_PRICE, MAX_PRICE),
-        'type': getRandomArrayItem(TYPE),
-        'rooms': getRandomIntByRange(MIN_ROOMS, MAX_ROOMS),
-        'guests': getRandomIntByRange(MIN_GUESTS, MAX_GUESTS),
-        'checkin': getRandomArrayItem(CHECKIN),
-        'checkout': getRandomArrayItem(CHECKOUT),
-        'features': getRandomArrayItem(FEATURES),
-        'description': '',
-        'photos': getRandomArrayItem(PHOTOS)
+      offer: {
+        title: getRandomArrayItem(TITLE),
+        address: 'location.x location.y',
+        price: getRandomIntByRange(MIN_PRICE, MAX_PRICE),
+        type: getRandomArrayItem(TYPE),
+        rooms: getRandomIntByRange(MIN_ROOMS, MAX_ROOMS),
+        guests: getRandomIntByRange(MIN_GUESTS, MAX_GUESTS),
+        checkin: getRandomArrayItem(CHECKIN),
+        checkout: getRandomArrayItem(CHECKOUT),
+        features: getRandomArrayItem(FEATURES),
+        description: '',
+        photos: getRandomArrayItem(PHOTOS)
       },
-      'location': {
-        'x': getRandomIntByRange(LOCATION_X_MIN, LOCATION_X_MAX),
-        'y': getRandomIntByRange(LOCATION_Y_MIN, LOCATION_Y_MAX)
+      location: {
+        x: getRandomIntByRange(LOCATION_X_MIN, LOCATION_X_MAX),
+        y: getRandomIntByRange(LOCATION_Y_MIN, LOCATION_Y_MAX)
       }
     });
   }
   return array;
 };
 
-var ads = createArreyOfObject(NUMBER_OF_ADS);
+// Принимает массив объектов-объявлений ads.
+// Создает копию разметки маркера из шаблона TemplatePin. Определяет положение маркера, адрес изображения и текстовое
+//  содержимое альтернативного изображения в соответсвии со свойствами объекта, входящего в массив ads.
+
+// Функции, к-ые что-то возрващают, принято называть с приставкой get (getPopupPin)?
+var renderPopupPin = function (ads) {
+  var popupPin = TemplatePin.cloneNode(true);
+
+  popupPin.style = 'left: ' + ads.location.x + 'px; top: ' + ads.location.y + 'px;';
+  popupPin.querySelector('img').src = ads.author.avatar;
+  popupPin.querySelector('img').alt = ads.offer.title;
+
+  return popupPin;
+};
+
+// Принимает массив объектов-объявлений ads.
+// Создает копию разметки объявления из шаблона TemplateAd. Определяет содержимое выбранных элементов в соответсвии со
+// свойствами объекта, входящего в массив ads.
+var renderAd = function (ads) {
+  var popupAd = TemplateAd.cloneNode(true);
+
+  popupAd.querySelector('.popup__title').textContent = ads.offer.title;
+  popupAd.querySelector('.popup__text--address').textContent = ads.offer.address;
+  popupAd.querySelector('.popup__text--price').textContent = ads.offer.price;
+  popupAd.querySelector('.popup__type').textContent = ads.offer.type;
+  popupAd.querySelector('.popup__text--capacity').textContent = ads.offer.rooms + ' для' + ads.offer.guests;
+  popupAd.querySelector('.popup__text--time').textContent =
+  'заезд после ' + ads.offer.checkin + ', выезд до ' + ads.offer.checkout;
+  popupAd.querySelector('.popup__features').textContent = ads.offer.features;
+  popupAd.querySelector('.popup__photos').textContent = ads.offer.photos;
+
+  return popupAd;
+};
+
+// Отрисовывает сгенерированные DOM-элементы в блок .map-pins.
+var createPins = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < ads.length; i++) {
+    fragment.appendChild(renderPopupPin(ads[i]));
+  }
+  pinList.appendChild(fragment);
+};
+
+// Отрисовывает сгенерированные DOM-элементы в блок .map(map) перед блоком .map__filters-cintainer(nextSibling).
+var createAds = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < ads.length; i++) {
+    fragment.appendChild(renderAd(ads[i]));
+  }
+  map.insertBefore(fragment, nextSibling);
+};
+
+// Функция корректировки острия маркера. Нужна ли она?
+
+var ads = getArreyOfObject(NUMBER_OF_ADS);
+createPins();
+createAds();
 
 if (map.classList.contains('map--faded')) {
   map.classList.remove('map--faded');
