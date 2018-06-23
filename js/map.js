e8e44e67df1dc2a8e11f7b4a4f66359696822ea4
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-06-23 14:24:29 +0300

    Page activation, fill in the address field, view similar ads

--- js/map.js
+++ js/map.js
@@ -24,12 +24,16 @@ var LOCATION_Y_MAX = 630;
 var MAP_PIN_WIDTH = 40;
 var MAP_PIN_HEIGHT = 40;
 
-var template = document.querySelector('template').content;
 var map = document.querySelector('.map');
 var pinList = document.querySelector('.map__pins');
+var template = document.querySelector('template').content;
 var templatePin = template.querySelector('.map__pin');
 var templateAd = template.querySelector('.map__card');
 var nextSibling = document.querySelector('.map__filters-container');
+var form = document.querySelector('.ad-form');
+var pinMain = document.querySelector('.map__pin--main');
+var address = document.getElementById('address');
+
 
 // Возвращает случайное целое число из диапозона min и max;
 var getRandomIntByRange = function (min, max) {
@@ -41,8 +45,8 @@ var getRandomArrayItem = function (arr) {
   return arr[getRandomIntByRange(0, arr.length - 1)];
 };
 
-// Принимает два числа. Возвращает значение от -0.5 до 0.5 (не включительно).
 // Используется в методе сортировки массива в случайном порядке - arr.sort(fn);
+// Принимает два элемента массива. Возвращает значение от -0.5 до 0.5 (не включительно).
 var getRandomOrder = function (_a, _b) {
   return Math.random() - 0.5;
 };
@@ -87,12 +91,25 @@ var getArreyOfObject = function (numberOfObject) {
 //  содержимое альтернативного изображения в соответсвии со свойствами объекта, входящего в массив ads.
 var renderPopupPin = function (ads) {
   var popupPin = templatePin.cloneNode(true);
-
   popupPin.style = 'left: ' + ads.location.x + 'px; top: ' + ads.location.y + 'px;';
   popupPin.querySelector('img').src = ads.author.avatar;
   popupPin.querySelector('img').alt = ads.offer.title;
-
   return popupPin;
+};
+
+// Принимает число. Склоняет существительное комната по числу комнат.
+var getDeclensionOfaRoom = function (number) {
+  if (number === 1) {
+    return ' комната для ';
+  } else if (number === 5) {
+    return ' комнат для ';
+  }
+  return ' комнаты для ';
+};
+
+// Принимает число. Склоняет существительное гость по числу гостей.
+var getDeclensionOfaGuests = function (number) {
+  return (number === 1) ? ' гостя' : ' гостей';
 };
 
 // Принимает массив объектов-объявлений ads.
@@ -100,18 +117,17 @@ var renderPopupPin = function (ads) {
 // свойствами объекта, входящего в массив ads.
 var renderAd = function (ads) {
   var popupAd = templateAd.cloneNode(true);
-
+  popupAd.querySelector('.popup__avatar').src = ads.author.avatar;
   popupAd.querySelector('.popup__title').textContent = ads.offer.title;
   popupAd.querySelector('.popup__text--address').textContent = ads.offer.address;
   popupAd.querySelector('.popup__text--price').textContent = ads.offer.price + 'Р/ночь';
   popupAd.querySelector('.popup__type').textContent = ads.offer.type;
   popupAd.querySelector('.popup__text--capacity').textContent = ads.offer.rooms + getDeclensionOfaRoom(ads.offer.rooms) +
-    ads.offer.guests + getDeclensionOfaGuests(ads.offer.guests);
+  ads.offer.guests + getDeclensionOfaGuests(ads.offer.guests);
   popupAd.querySelector('.popup__text--time').textContent =
   'заезд после ' + ads.offer.checkin + ', выезд до ' + ads.offer.checkout;
   popupAd.querySelector('.popup__features').textContent = ads.offer.features;
   popupAd.querySelector('.popup__photos').textContent = ads.offer.photos;
-
   return popupAd;
 };
 
@@ -133,25 +149,94 @@ var createAds = function () {
   map.insertBefore(fragment, nextSibling);
 };
 
-// Принимает число. Склоняет существительное комната по числу комнат.
-var getDeclensionOfaRoom = function (number) {
-  if (number === 1) {
-    return ' комната для ';
-  } else if (number === 5) {
-    return ' комнат для ';
-  }
-  return ' комнаты для ';
-};
-
-// Принимает число. Склоняет существительное гость по числу гостей.
-var getDeclensionOfaGuests = function (number) {
-  return (number === 1) ? ' гостя' : ' гостей';
-};
-
 var ads = getArreyOfObject(NUMBER_OF_ADS);
 createPins();
 createAds();
 
-if (map.classList.contains('map--faded')) {
-  map.classList.remove('map--faded');
+
+// 4 задание.
+
+// Активирует карту, при перетаскивании метки и добавляет координаты метки в поле адреса.
+pinMain.addEventListener('mouseup', function (evt) {
+  if (map.classList.contains('map--faded')) {
+    map.classList.remove('map--faded');
+  }
+  if (form.classList.contains('ad-form--disabled')) {
+    form.classList.remove('ad-form--disabled');
+  }
+  address.value = evt.clientX + ' ' + evt.clientY;
+});
+
+
+var pinButtonList = map.querySelectorAll('.map__pin:not(.map__pin--main)');
+var adList = map.querySelectorAll('.popup');
+var adButtonClose = map.querySelectorAll('.popup__close');
+
+
+// скрыл все объявления.
+for (var i = 0; i < adList.length; i++) {
+  adList[i].style = 'display: none;';
 }
+
+// Принимает индекс. Скрывает все объявления и показывет объявление с входящим индексом.
+var showAd = function (index) {
+  for (i = 0; i < adList.length; i++) {
+    adList[i].style = 'display: none;';
+  }
+  adList[index].style = 'display: block;';
+};
+
+// Принимет индекс. Скрывает объявление с входящим индексом.
+var hideAd = function (index) {
+  adList[index].style = 'display: none;';
+};
+
+// Принимает событие-event
+// Если клик по маркеру, то показывает объявление с индексом, соотв. индексу маркера, по к-му был клик.
+// Если клик по кнопке закрыть, то закрывает объявление с индексом, соотв. индексу кнопке-закрыть.
+
+var onMapClick = function (evt) {
+  for (i = 0; i < pinButtonList.length; i++) {
+    if (pinButtonList[i] === evt.target) {
+      showAd(i);
+    }
+  }
+  for (i = 0; i < adButtonClose.length; i++) {
+    if (adButtonClose[i] === evt.target) {
+      hideAd(i);
+    }
+  }
+};
+
+// Обработчик клика по карте.
+map.addEventListener('click', onMapClick);
+
+
+// Принимает массив и элемент-target
+// Возвращает элемент массива, если он соответсвует элементу-target
+// var getClickedElement = function (arr, target) {
+//   for(var i = 0; i < arr.length; i++) {
+//     if (arr[i] === target) {
+//       return arr[i];
+//     }
+//   }
+// };
+
+// var onMapClick = function (evt) {
+//   switch (evt.target) {
+//     case кнопка из pinButtonList: 'показать соответвующее окно из adList';
+//       break;
+//     case кнопка из adButtonClose: 'закрыть соотв окно adList'
+//       break;
+//   }
+// };
+
+// var onMapClick = function (evt) {
+
+//   switch (evt.target) {
+//     case getClickedElement(pinButtonList, evt.target): showAd();
+//       break;
+//     case getClickedElement(adButtonClose, evt.target): hideAd();
+//       break;
+//   }
+// };
