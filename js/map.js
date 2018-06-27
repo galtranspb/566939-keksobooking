3c0918024e6a124ba504ba8f9c6304f4ae102562
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-06-27 21:21:29 +0300

    Fixed display of photos in the ad: added function copyNewChildren

--- js/map.js
+++ js/map.js
@@ -145,12 +145,22 @@ var createNewChildren = function (obj, arr) {
   }
 };
 
+// Принимает объект и массив. Удаляет все дочерние элементы объекта. Клонирует новые элементы из шаблона, в количестве
+// равном длине массива. Назначает свойство src i-му элементу объекта, равное значению i-го элемента массива.
+var copyNewChildren = function (obj, arr) {
+  deleteChildren(obj);
+  for (var i = 0; i < arr.length; i++) {
+    var element = templateAd.querySelector('.popup__photo').cloneNode();
+    element.src = arr[i];
+    obj.appendChild(element);
+  }
+};
+
 // Принимает массив объектов-объявлений ads.
 // Создает копию разметки объявления из шаблона TemplateAd. Определяет содержимое выбранных элементов, в соответсвии со
 // свойствами объекта.
 var renderAd = function (obj) {
   var popupAd = templateAd.cloneNode(true);
-  var features = popupAd.querySelector('.popup__features');
   popupAd.style = 'display: none;';
   popupAd.querySelector('.popup__avatar').src = obj.author.avatar;
   popupAd.querySelector('.popup__title').textContent = obj.offer.title;
@@ -161,8 +171,8 @@ var renderAd = function (obj) {
   obj.offer.guests + getDeclensionOfaGuests(obj.offer.guests);
   popupAd.querySelector('.popup__text--time').textContent =
   'заезд после ' + obj.offer.checkin + ', выезд до ' + obj.offer.checkout;
-  createNewChildren(features, obj.offer.features);
-  popupAd.querySelector('.popup__photos').textContent = obj.offer.photos;
+  createNewChildren(popupAd.querySelector('.popup__features'), obj.offer.features);
+  copyNewChildren(popupAd.querySelector('.popup__photos'), obj.offer.photos);
   return popupAd;
 };
 
