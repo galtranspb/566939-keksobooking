b3dccdf2d9cccbf330be63b823f47df1184e1201
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-07-27 11:42:04 +0300

    Can't combine the 'change' functions into a single function

--- js/avatar.js
+++ js/avatar.js
@@ -4,40 +4,44 @@
 
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-  // ******************************загрузка аватарки******************************************
-
-  var fileChooser = document.querySelector('.ad-form-header__input');
+  var avatarChooser = document.querySelector('.ad-form-header__input');
   var preview = document.querySelector('.ad-form-header__preview img');
+  var photoChooser = document.querySelector('.ad-form__input');
+  var photoList = document.querySelector('.ad-form__photo-container');
 
-  var onFileChooserChange = function () {
-    var file = fileChooser.files[0];
+  var makeElement = function (tagName, className) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+    return element;
+  };
+
+  var onAvatarLoad = function (evt) {
+    preview.src = evt.target.result;
+  };
+
+  var onPhotoLoad = function (evt) {
+    var item = makeElement('img', 'ad-form__photo');
+    item.src = evt.target.result;
+    photoList.appendChild(item);
+  };
+
+  var onAvatarChange = function () {
+    var file = avatarChooser.files[0];
     var fileName = file.name.toLowerCase();
 
-    var matches = FILE_TYPES.some(function (el) {
-      return fileName.endsWith(el);
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
     });
 
     if (matches) {
       var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        preview.src = reader.result;
-      });
-
+      reader.addEventListener('load', onAvatarLoad);
       reader.readAsDataURL(file);
     }
   };
 
-  fileChooser.addEventListener('change', onFileChooserChange);
-
-  // *************************************закгрузка фото жилья********************************
-
-  var fileChooser2 = document.querySelector('.ad-form__input');
-  var previewList = document.querySelector('.ad-form__photo-container');
-  previewList.removeChild(document.querySelector('div.ad-form__photo'));
-
-  var onFileChooserChange2 = function () {
-    var fileList = fileChooser2.files;
+  var onPhotoChange = function () {
+    var fileList = photoChooser.files;
 
     for (var i = 0; i < fileList.length; i++) {
       var fileName = fileList[i].name.toLowerCase();
@@ -47,22 +51,15 @@
       });
 
       if (matches) {
-
         var reader = new FileReader();
-
-        reader.addEventListener('load', function (evt) {
-          var dataUri = evt.target.result;
-          var img = document.createElement('img');
-          img.src = dataUri;
-          img.classList.add('ad-form__photo');
-          previewList.appendChild(img);
-        });
-
+        reader.addEventListener('load', onPhotoLoad);
         reader.readAsDataURL(fileList[i]);
       }
     }
   };
 
-  fileChooser2.addEventListener('change', onFileChooserChange2);
+  photoList.removeChild(document.querySelector('div.ad-form__photo'));
+  avatarChooser.addEventListener('change', onAvatarChange);
+  photoChooser.addEventListener('change', onPhotoChange);
 
 })();
